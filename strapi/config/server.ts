982ca4697de1b2cd2fd22a2527c5c5ca100f2ece
GodSpeed <godspeed@ZeroMotions-MacBook-Air.local>
2025-07-17 14:17:24 -0500

    feat(strapi): configure public url for images and webhooks

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://dynamic-authority-ae351ff389.strapiapp.com'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
